622d05715899c5eee02eaf6ca32bd29b0484de83
Yash Pandey <pandeyyash041@gmail.com>
2025-05-06 14:52:34 +0530

    Configured JSON and cookie parsing, and linked MongoDB Atlas to support controller functionality

--- src/app.js
+++ src/app.js
@@ -1,10 +1,18 @@
 import express from "express";
-
-const app = express();
+import cookieParser from "cookie-parser";
 
 //router imports
 import healthCheckRouter from "./routes/healthcheck.routes.js";
+import router from "./routes/auth.routes.js";
+
+const app = express();
+
+// Important To Read The JSON & Form data.....
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 app.use("/api/v1/healthcheck", healthCheckRouter);
+app.use("/api/v1/auth", router);
 
 export default app;
--- src/db/index.js
+++ src/db/index.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const DB_Connect = async () => {
   try {
     // As Database In In Another Continent.....
-    await mongoose.connect(process.env.MONGO_URI);
+    await mongoose.connect(process.env.MONGO_URI_ATLAS);
     console.log("MongoDB Connected");
   } catch (error) {
     console.error("ERROR While Connecting With MongoDB", error);
