ea1cbe42839e9e328bb6e2a9fc9b26cb2ee2e2f8
Yash Pandey <pandeyyash041@gmail.com>
2025-05-06 17:05:29 +0530

    Implemented robust error handling in TaskNexus using a custom ApiError class for structured error throwing across the application. Added a centralized error-handling middleware to format all errors uniformly, improving clarity and consistency in client-side error responses.

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import cookieParser from "cookie-parser";
 
 //router imports
 import healthCheckRouter from "./routes/healthcheck.routes.js";
-import router from "./routes/auth.routes.js";
+import authorizationRoute from "./routes/auth.routes.js";
 
 const app = express();
 
@@ -12,7 +12,24 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
 
+// Error Handling Middleware Which Will Handle All The Errors In The Application.....
+const errorMiddleware = (err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+
+  res.status(statusCode).json({
+    statusCode: statusCode,
+    message: err.message || "Internal Server Error",
+    success: false,
+    errors: err.errors || [],
+    stack: process.env.NODE_ENV === "development" ? err.stack : undefined, // optional
+  });
+};
+
 app.use("/api/v1/healthcheck", healthCheckRouter);
-app.use("/api/v1/auth", router);
+app.use("/api/v1/auth", authorizationRoute);
+
+// Finally adding the error handler at the very bottom
+// This Will Improve The Readability Of The Api-Errors.....
+app.use(errorMiddleware);
 
 export default app;
