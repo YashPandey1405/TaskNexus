67e116a428a5a97a4e21827b121d5021c1b9233a
Yash Pandey <pandeyyash041@gmail.com>
2025-06-24 12:34:15 +0530

    Configured The Root Page To Render Login Page When User Isn't Logged In Else Render Home Page COmponent

--- frontend/src/routes/__root.jsx
+++ frontend/src/routes/__root.jsx
@@ -1,11 +1,44 @@
 import * as React from "react";
 import { Outlet, createRootRoute } from "@tanstack/react-router";
+import { useRouter, Link } from "@tanstack/react-router";
+import { useEffect } from "react";
+import { authStore } from "../store/authStore.js";
 
 export const Route = createRootRoute({
   component: RootComponent,
 });
 
 function RootComponent() {
+  const router = useRouter();
+
+  const isLoggedInZustand = authStore((state) => state.isLoggedIn);
+  const loggedInUserIdZudtand = authStore((state) => state.loggedInUserId);
+  console.log("User ID:", isLoggedInZustand);
+  console.log("User ID:", loggedInUserIdZudtand);
+
+  // Redirect to login if not logged in , Else Redirect to home.....
+  useEffect(() => {
+    const getRedirect = async () => {
+      try {
+        // Redirect early if not logged in
+        if (!isLoggedInZustand) {
+          console.log("User is not logged in, redirecting to login page...");
+          router.navigate({ to: "/login" });
+          return; // stop further execution
+        }
+        router.navigate({ to: "/home" });
+        return;
+      } catch (error) {
+        setData({
+          success: false,
+          message: "getProjects failed. Try again later.",
+        });
+      }
+    };
+
+    getRedirect();
+  }, [router, isLoggedInZustand]);
+
   return (
     <React.Fragment>
       <Outlet />
